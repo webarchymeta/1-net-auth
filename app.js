71ff1ca4b2817ac4a548825ea9c4b1f463c128c6
Shuqian Ying <sqying@archymeta.com>
2016-05-21 07:04:29 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+const _ = require('lodash'),
+    express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    ejs = require('ejs'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('cookie-session'),
+    config = require('./config/config'),
+    userStore = require(path.join(__dirname, 'lib/user-store')),
+    oauth = require(path.join(__dirname, 'lib/1-net-oauth2'));
+
+oauth.initialize(config, userStore);
+
+const app = express();
+const routes = require('./routes/index');
+
+if (process.cwd() !== __dirname) {
+    process.chdir(__dirname);
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.html', ejs.__express);
+app.set('view options', { layout: false });
+app.set('view engine', 'ejs');
+
+app.use(session({
+    keys: config.cookieSessionKeys
+}));
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+require('./lib/error-catchers')(app);
+
+let server = app.listen(config.port, config.ip, undefined,  () => {
+    console.log('Express server listening on port ' + config.port + ' at ' + config.ip);
+});
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+let express = require('express'),
+    router = express.Router(),
+    path = require('path'),
+    config = require(path.join(__dirname, '../config/config')),
+    oauth = require(path.join(__dirname, '../lib/1-net-oauth2')),
+    userStore = require(path.join(__dirname, '../lib/user-store'));
+
+let auth =  new oauth();
+
+router.get(config.oauth.returnPath, (req, res, next) => {
+    auth.callback(req).then(result => {
+        if (!result.ok) {
+            next('login failed');
+        }
+        return auth.login(req, result).then(() => {
+            res.redirect(result.returnTo);
+        });
+    }).catch(() => {
+        next('login failed');
+    }).done();
+});
+
+router.get('/logout', auth.check(), auth.logout);
+
+router.get('/login', auth.check(), (req, res) => {
+    res.redirect('/');
+});
+
+router.get('/admin', auth.check('admin_page_acl'), (req, res) => {
+    userStore.accessControlList('admin_page_acl').then((acl) => {
+        res.render('admin.html', { title: 'Administration page', acl: acl, login: req.user });
+    });
+});
+
+router.get('/work', auth.check(), (req, res) => {
+    res.render('work.html', { title: 'Work page', login: req.user });
+});
+
+router.get('/', (req, res) => {
+    res.render('index.html', { title: '1-NET sign in demo', login: req.session.user });
+});
+
+module.exports = router;
