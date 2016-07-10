d6a306c8845e4ef27abc4167c6fac31261f0a1cc
Shuqian Ying <sqying@archymeta.com>
2016-07-11 04:22:52 +0800

    add call to get current endpoint info api after login, bug fixes

--- routes/index.js
+++ routes/index.js
@@ -34,20 +34,20 @@ router.get('/login', auth.check(), (req, res) => {
 
 router.get('/admin', auth.check('admin_page_acl'), (req, res) => {
     userStore.getAccessControl('admin_page_acl').then((acl) => {
-        res.render('admin.html', { title: 'Administration page', acl: acl, login: req.isAuthenticated() });
+        res.render('admin.html', { title: 'Administration page', acl: acl, login: req.isAuthenticated() ? req.user : undefined });
     });
 });
 
 router.get('/work', auth.check(), (req, res) => {
-    res.render('work.html', { title: 'Work page', login: req.isAuthenticated() });
+    res.render('work.html', { title: 'Work page', login: req.isAuthenticated() ? req.user : undefined });
 });
 
 router.get('/readme', (req, res, next) => {
-    res.render('readme.html', { login: req.isAuthenticated() });
+    res.render('readme.html', { login: req.isAuthenticated() ? req.user : undefined });
 });
 
 router.get('/', (req, res) => {
-    res.render('index.html', { title: '1-NET sign in demo', login: req.isAuthenticated() });
+    res.render('index.html', { title: '1-NET sign in demo', login: req.isAuthenticated() ? req.user : undefined });
 });
 
 module.exports = router;
